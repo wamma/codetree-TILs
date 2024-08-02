ac1020fa4b531f1eddd1eadd61713f40f9bf20e3
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-08-02 16:19:48 +0900

    Title: 배열의 특정 위치 찾기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240802/배열의 특정 위치 찾기/find-specific-location-fo-array.cpp
+++ 240802/배열의 특정 위치 찾기/find-specific-location-fo-array.cpp
@@ -2,24 +2,25 @@
 using namespace std;
 
 int main() {
-    int arr[10], sum1 = 0, sum2 = 0, count = 0;
+    int arr[10], even_sum = 0, multiple_of_3_sum = 0, multiple_of_3_count = 0;
+    
     for (int i = 0; i < 10; ++i)
         cin >> arr[i];
-    for (int i = 0; i < 10; ++i)
-    {
-        if (arr[i] % 2 == 0)
-            sum1 += arr[i];
-        else if ((i + 1) % 3 == 0)
-        {
-            sum2 += arr[i];
-            count++;
+    
+    for (int i = 0; i < 10; ++i) {
+        if ((i + 1) % 2 == 0) // 짝수 번째 원소 (1-based index)
+            even_sum += arr[i];
+        if ((i + 1) % 3 == 0) { // 3의 배수 번째 원소 (1-based index)
+            multiple_of_3_sum += arr[i];
+            multiple_of_3_count++;
         }
     }
-    double sum3 = (double)sum2 / count;
-
+    
+    double multiple_of_3_avg = (multiple_of_3_count == 0) ? 0 : (double)multiple_of_3_sum / multiple_of_3_count;
+    
     cout << fixed;
     cout.precision(1);
-    cout << sum1 << " " << sum3;
+    cout << even_sum << " " << multiple_of_3_avg << endl;
 
     return 0;
 }
