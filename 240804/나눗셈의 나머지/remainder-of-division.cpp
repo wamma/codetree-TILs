6183b533681a162b5d611e9433b7f5d1ee6a58a9
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-08-04 19:25:01 +0900

    Title: 나눗셈의 나머지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240804/나눗셈의 나머지/remainder-of-division.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, divNum;
+    cin >> num >> divNum;
+
+    int arr[10] = {};
+    while (num > 0)
+    {
+        int namuji = num % divNum;
+        arr[namuji]++;
+        num /= divNum;
+    }
+    int result = 0;
+    for (int i = 0; i < divNum; ++i)
+    {
+        int count = arr[i];
+        result += count * count;
+    }
+    cout << result;
+    return 0;
+}
