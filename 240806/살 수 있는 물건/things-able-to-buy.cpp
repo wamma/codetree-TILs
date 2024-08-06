a66c9a797026b24b5971ae4db54225bccd2b9c32
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-08-06 16:14:52 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240806/살 수 있는 물건/things-able-to-buy.cpp
+++ 240806/살 수 있는 물건/things-able-to-buy.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    cout << ((n >= 3000) ? "book" : "mask") << endl;
+    if (n >= 3000)
+        cout << "book";
+    else if (n >= 1000)
+        cout << "mask";
+    else
+        cout << "no";
     return 0;
 }
