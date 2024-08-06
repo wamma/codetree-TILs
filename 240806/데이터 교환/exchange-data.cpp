8b2fb0726a9956f9ba73acda625d9fbf35197227
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-08-06 15:54:34 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240806/데이터 교환/exchange-data.cpp
+++ 240806/데이터 교환/exchange-data.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main() {
     int a = 5, b = 6, c = 7;
-    b = a;
+    int tmp;
+    tmp = a;
+    b = tmp;
     c = b;
-    a = c;
     cout << a << '\n';
     cout << b << '\n';
     cout << c << '\n';
