890bf0cdd4fff872d721fed0d98fc1c47971859b
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-08-06 15:51:22 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240806/두 실수의 곱/the-product-of-two-real-numbers.cpp
+++ 240806/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
-    int a = 5.26, b = 8.27;
-    cout << a * b;
+    double a = 5.26, b = 8.27;
+    double result = a * b;
+    cout << fixed << setprecision(3) << result;
     return 0;
 }
