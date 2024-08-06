ff1459ea9788420c6b3a322449f2c7271b53249f
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-08-06 19:28:12 +0900

    Title: 세 수의 중간값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240806/세 수의 중간값/median-of-three-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (b > a && b < c)
+        cout << 1;
+    else
+        cout << 0;
+    return 0;
+}
