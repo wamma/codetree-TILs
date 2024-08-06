30ed9b671be27fb009b9ae30dd7cfc113fef8b35
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-08-06 15:56:09 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240806/데이터 교환/exchange-data.cpp
+++ 240806/데이터 교환/exchange-data.cpp
@@ -3,12 +3,17 @@ using namespace std;
 
 int main() {
     int a = 5, b = 6, c = 7;
-    int tmp;
-    tmp = a;
-    b = tmp;
-    c = b;
-    cout << a << '\n';
-    cout << b << '\n';
-    cout << c << '\n';
+
+    // 변수 값 변경
+    int temp = a; // 임시 변수에 a 값을 저장
+    a = c;        // a에 c 값을 대입
+    c = b;        // c에 b의 값을 대입
+    b = temp;     // b에 저장된 a의 값을 대입
+
+    // 결과 출력
+    cout << a << '\n'; // a의 값 (c의 값)
+    cout << b << '\n'; // b의 값 (a의 값)
+    cout << c << '\n'; // c의 값 (b의 값)
+
     return 0;
 }
